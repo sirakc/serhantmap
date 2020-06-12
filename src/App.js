b6827825a3a2124ca662b44f93cf25dfc1adfda3
Abhishek Kumar Chowdhury <bkp.abhi@gmail.com>
2020-06-12 19:37:07 +0530

    yolo2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,12 @@ class App extends React.Component {
         <div style={{ width: '100vw', height: '100vh' }}>
           <ReactGlobe
             globeOptions={{
-              texture: 'https://serving.photos.photobox.com/048522852493b7e952dfa462698c61315fc4bc71c601fb06d362635f0914453f0b23947c.jpg'
+              texture: 'https://raw.githubusercontent.com/sirakc/serhantmap/master/public/serhant_earth.jpg'
+            }}
+            lightOptions={{
+              pointLightColor: 'white',
+              pointLightIntensity: 0,
+              pointLightPositionRadiusScales: [2, 1, -1],
             }}
           />
         </div>
